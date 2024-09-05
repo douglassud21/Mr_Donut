a2c5734170c95cd03a9033fa609c772c08d9761b
Douglas <douglas.nascimentosud21@gmail.com>
2024-09-04 23:34:24 -0300

    .

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ video2.addEventListener('ended', () => {
     video1.style.display = 'block';  // Mostra o primeiro vídeo novamente
     video1.play();  // Reproduz o primeiro vídeo novamente
 });
+
+
+
+// Seleciona o menu hambúrguer e o menu mobile
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// Adiciona um evento de clique ao menu hambúrguer
+hamburger.addEventListener('click', () => {
+  // Alterna a classe 'active' no menu hambúrguer
+  hamburger.classList.toggle('active');
+  // Alterna a classe 'active' no menu mobile
+  mobileMenu.classList.toggle('active');
+});
+
+// Seleciona todas as opções do menu mobile
+const menuLinks = document.querySelectorAll('.mobile-menu a');
+
+// Adiciona um evento de clique a cada opção do menu
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Remove a classe 'active' do menu hambúrguer e do menu mobile
+    hamburger.classList.remove('active');
+    mobileMenu.classList.remove('active');
+  });
+});
+
